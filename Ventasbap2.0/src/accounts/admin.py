fd5579b01b832e2f588df32400765061a55588af
julio Mejias <julionml78@gmail.com>
2018-06-17 21:03:30 -0400

    actualizacion de los requerimientos faltantes se añadio perfil al usuario se añadio confirmacion por email se añadio recuperacion de contraseña se añadio metodo de pago con strip(temporal) se añadio integracion con mailchimp para email marketing se añadieron estadisticas sencillas se añadio "estatus de la orden"

--- Ventasbap2.0/src/accounts/admin.py
+++ Ventasbap2.0/src/accounts/admin.py
@@ -6,7 +6,7 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 
 from .forms import UserAdminCreationForm, UserAdminChangeForm
-from .models import GuestEmail
+from .models import GuestEmail, EmailActivation
 
 User = get_user_model()
 
@@ -21,10 +21,10 @@ class UserAdmin(BaseUserAdmin):
     # These override the definitions on the base UserAdmin
     # that reference specific fields on auth.User.
     list_display = ('email', 'admin')
-    list_filter = ('admin', 'staff', 'active')
+    list_filter = ('admin', 'staff', 'is_active')
     fieldsets = (
         (None, {'fields': ('full_name', 'email', 'password')}),
-        ('Personal info', {'fields': ('city', 'ci', 'sex', 'age', 'cellphone')}),
+        ('Personal info', {'fields': ('city', 'ci', 'sex', 'bday', 'cellphone')}),
         ('Permissions', {'fields': ('admin', 'staff', 'active',)}),
     )
     # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
@@ -45,6 +45,15 @@ admin.site.register(User, UserAdmin)
 # Remove Group Model from admin. We're not using it.
 admin.site.unregister(Group)
 
+
+
+class EmailActivationAdmin(admin.ModelAdmin):
+    search_fields = ['email']
+    class Meta:
+        model = EmailActivation
+
+
+admin.site.register(EmailActivation, EmailActivationAdmin)
 
 
 class GuestEmailAdmin(admin.ModelAdmin):
--- Ventasbap2.0/src/carts/views.py
+++ Ventasbap2.0/src/carts/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
 
@@ -5,13 +6,20 @@ from django.shortcuts import render, redirect
 from accounts.forms import LoginForm, GuestForm
 from accounts.models import GuestEmail
 
-from addresses.forms import AddressForm
+from addresses.forms import AddressCheckoutForm
 from addresses.models import Address
 
 from billing.models import BillingProfile
 from orders.models import Order
 from products.models import Product
 from .models import Cart
+
+
+import stripe
+STRIPE_SECRET_KEY = getattr(settings, "STRIPE_SECRET_KEY", "sk_test_cu1lQmcg1OLffhLvYrSCp5XE")
+STRIPE_PUB_KEY =  getattr(settings, "STRIPE_PUB_KEY", 'pk_test_PrV61avxnHaWIYZEeiYTTVMZ')
+stripe.api_key = STRIPE_SECRET_KEY
+
 
 
 def cart_detail_api_view(request):
@@ -68,14 +76,19 @@ def checkout_home(request):
     if cart_created or cart_obj.products.count() == 0:
         return redirect("cart:home")  
     
-    login_form = LoginForm()
-    guest_form = GuestForm()
-    address_form = AddressForm()
+    login_form = LoginForm(request=request)
+    guest_form = GuestForm(request=request)
+    address_form = AddressCheckoutForm()
     billing_address_id = request.session.get("billing_address_id", None)
+
+    shipping_address_required = not cart_obj.is_digital
+
+
     shipping_address_id = request.session.get("shipping_address_id", None)
 
     billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
     address_qs = None
+    has_card = False
     if billing_profile is not None:
         if request.user.is_authenticated():
             address_qs = Address.objects.filter(billing_profile=billing_profile)
@@ -88,15 +101,26 @@ def checkout_home(request):
             del request.session["billing_address_id"]
         if billing_address_id or shipping_address_id:
             order_obj.save()
+        has_card = billing_profile.has_card
 
     if request.method == "POST":
         "check that order is done"
-        is_done = order_obj.check_done()
-        if is_done:
-            order_obj.mark_paid()
-            request.session['cart_items'] = 0
-            del request.session['cart_id']
-            return redirect("cart:success")
+        is_prepared = order_obj.check_done()
+        if is_prepared:
+            did_charge, crg_msg = billing_profile.charge(order_obj)
+            if did_charge:
+                order_obj.mark_paid() # sort a signal for us
+                request.session['cart_items'] = 0
+                del request.session['cart_id']
+                if not billing_profile.user:
+                    '''
+                    is this the best spot?
+                    '''
+                    billing_profile.set_cards_inactive()
+                return redirect("cart:success")
+            else:
+                print(crg_msg)
+                return redirect("cart:checkout")
     context = {
         "object": order_obj,
         "billing_profile": billing_profile,
@@ -104,6 +128,9 @@ def checkout_home(request):
         "guest_form": guest_form,
         "address_form": address_form,
         "address_qs": address_qs,
+        "has_card": has_card,
+        "publish_key": STRIPE_PUB_KEY,
+        "shipping_address_required": shipping_address_required,
     }
     return render(request, "carts/checkout.html", context)
 
